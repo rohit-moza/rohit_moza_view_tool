8a0649945bc6e2dfe16e6cb7202c4df288c844c0
Rohit <moza.rohit@gmail.com>
2019-01-26 08:33:08 -0500

    Dev tool initial commit

---
+++ lib/rohit_moza_view_tool.rb
@@ -0,0 +1,6 @@
+require "rohit_moza_view_tool/version"
+require "rohit_moza_view_tool/renderer"
+
+module RohitMozaViewTool
+  # Your code goes here...
+end
---
+++ lib/rohit_moza_view_tool/version.rb
@@ -0,0 +1,3 @@
+module RohitMozaViewTool
+  VERSION = "0.1.0"
+end
